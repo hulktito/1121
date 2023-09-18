c1389d586cc0ec26d0799df6e9ec921473b0c6aa
youness tair <106333488+hulktito@users.noreply.github.com>
2023-09-18 17:59:13 +0000

    Add files via upload

---
+++ ngrok-rdp-main/resources/start.bat
@@ -0,0 +1,22 @@
+@echo off
+del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By venom_bolti_public" > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
+net user venom_bolti_public Admin@null /add >nul
+net localgroup administrators venom_bolti_public /add >nul
+net user venom_bolti_public /active:yes >nul
+net user installer /delete
+diskperf -Y >nul
+sc config Audiosrv start= auto >nul
+sc start audiosrv >nul
+ICACLS C:\Windows\Temp /grant venom_bolti_public:F >nul
+ICACLS C:\Windows\installer /grant venom_bolti_public:F >nul
+echo Successfully installed! If RDP is dead, rebuild again.
+echo IP:
+tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
+echo Username: venom_bolti_public
+echo Password: Admin@null
+echo You can login now
+echo RDP By @venom_bolti_public
+ping -n 10 127.0.0.1 >nul
